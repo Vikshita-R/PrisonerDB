dbc79ccccbed98015d32ab72b111ce7b3dcec39a
Aryan R <72093151+A-a2008@users.noreply.github.com>
2024-01-27 03:01:54 +0530

    Add Crime and Court

--- main/urls.py
+++ main/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("update/prisoner/", views.select_prisoner, name="select_prisoner"),
     path("update/prisoner/<int:prisoner_id>/", views.prisoner_details_boxes, name="prisoner_details_boxes"),
     path("update/add/prisoner/crime/<int:prisoner_id>/", views.crime_data, name="add_crime_fir"),
-    path("update/add/prisoner/court/<int:prisoner_id>/", views.court_data, name="add_court"),
+    path("update/add/prisoner/court/<int:prisoner_id>/", views.court_data, name="add_court")
 ]
